69308fd10a049a83d68a25d02e45f2ab7821b296
Cong-The <88782485+Cong-The@users.noreply.github.com>
2021-11-02 13:16:50 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -18,49 +18,63 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvtData;
-    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.edt_data);
         Button btnPushData = findViewById(R.id.btn_push_data);
         Button btnGetData = findViewById(R.id.btn_get_data);
         tvtData = findViewById(R.id.tvt_get_data);
 
         btnPushData.setOnClickListener(view -> onClickPushData());
         btnGetData.setOnClickListener(view -> onClickgetData());
+        Button btnDel = findViewById(R.id.btn_delete_data);
+        btnDel.setOnClickListener(view -> OnClickDelData());
     }
 
     private void onClickPushData() {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("User_info");
 
-        myRef.setValue(Integer.parseInt(editText.getText().toString().trim()), new DatabaseReference.CompletionListener(){
+        User user = new User(1,"Name1",new Job(1,"job1"));
+        user.setAddress("HCM");
+        myRef.setValue(user, new DatabaseReference.CompletionListener() {
             @Override
             public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-                Toast.makeText(MainActivity.this,"Push data success",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Push Data success", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
     private void onClickgetData(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("User_info");
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                int value = dataSnapshot.getValue(Integer.class);
-                tvtData.setText(String.valueOf(value));
+                User user = dataSnapshot.getValue(User.class);
+                tvtData.setText(user.toString());
 
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
 
+            }
+        });
+    }
+
+    private void OnClickDelData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.removeValue(new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(MainActivity.this,"Delete data success",Toast.LENGTH_SHORT).show();
             }
         });
     }
